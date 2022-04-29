d159c33579f0bd536ddb5788f11a41ce2f48af43
Mohd Shahid <sidmirza4@gmail.com>
2022-04-29 15:09:11 +0530

    edit base files and logo

--- src/layouts/base.js
+++ src/layouts/base.js
@@ -6,7 +6,7 @@ const BaseLayout = ({ children }) => {
   return (
     <div id="main">
       <Head>
-        <title>Junior García | Web & Mobile developer</title>
+        <title>Mohd Shahid | Web & Mobile developer</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
@@ -29,22 +29,22 @@ const BaseLayout = ({ children }) => {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@jrgarciadev" />
-        <meta property="og:site_name" content="Junior García | Web & Mobile developer" />
+        <meta name="twitter:site" content="@sidmirza4" />
+        <meta property="og:site_name" content="Mohd Shahid | Web & Mobile developer" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Junior García | Web & Mobile developer" />
+        <meta property="og:title" content="Mohd Shahid | Web & Mobile developer" />
         <meta property="og:locale" content="en" />
-        <meta property="og:url" content="https://jrgarciadev.com" />
+        <meta property="og:url" content="https://mirzashahid.com" />
         <meta
           name="description"
           content="Hello! I'm Junior, a Software Developer based in Buenos Aires, Argentina. I enjoy creating beautiful and reliable applications for internet and phones. My goal is to always build scalable products and performant experiences."
         />
         <meta
           name="keywords"
-          content="Developer, Javascript, Freelancer, React, React native, PHP, Laravel Developer, Vue JS Developer, React Developer"
+          content="Developer, Javascript, Freelancer, React, Typescript, NextJS, NestJS, MongoDB, React Developer"
         />
-        <meta property="og:image" content="https://jrgarciadev.com/avatar.jpeg" />
-        <meta property="twitter:image" content="https://jrgarciadev.com/avatar.jpeg" />
+        <meta property="og:image" content="https://mirzashahid.com/avatar.jpeg" />
+        <meta property="twitter:image" content="https://mirzashahid.com/avatar.jpeg" />
       </Head>
       {children}
     </div>
